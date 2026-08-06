9e8d6ed7bdb8a60665e7c8b0e7cd1753a0a8fa5c
malek-chermiti <malek02chermiti@gmail.com>
2026-08-07 00:03:43 +0100

    modify model user

--- services/users/models.py
+++ services/users/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, PrimaryKeyConstraint, String, UniqueConstraint, text
+from sqlalchemy import Integer, String, text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
@@ -7,13 +7,9 @@ class Base(DeclarativeBase):
 
 class Users(Base):
     __tablename__ = 'users'
-    __table_args__ = (
-        PrimaryKeyConstraint('email', name='users_pkey'),
-        UniqueConstraint('id', name='users_id_key'),
-    )
 
-    id: Mapped[int] = mapped_column(Integer, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     username: Mapped[str] = mapped_column(String(50), nullable=False)
-    email: Mapped[str] = mapped_column(String(100), primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
     role: Mapped[str] = mapped_column(String(20), nullable=False, server_default=text("'operateur'::character varying"))
